06665c30b0234f3e288daf989cc476ce7d0ce1ff
Kemjiga <kemerjg@gmail.com>
2025-08-11 11:46:08 -0500

    feat: add enums and responses

---
+++ src/orders/common/responses.ts
@@ -0,0 +1,18 @@
+import { OrderStatus } from './enums';
+
+export interface OrderItemResponse {
+  id: number;
+  description: string;
+  quantity: number;
+  unitPrice: number;
+}
+
+export interface OrderResponse {
+  id: number;
+  clientName: string;
+  status: OrderStatus;
+  total: number;
+  items: OrderItemResponse[];
+  createdAt: string;
+  updatedAt: string;
+}
